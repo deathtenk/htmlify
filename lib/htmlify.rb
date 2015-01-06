7ac768f520f19dec2622c9a0b3d29998a238eafe
Henk Reder <henk@anestuary.com>
2015-01-06 16:44:48 -0500

    refactored 'div' method to be called html, now i provide the tag as the first argument, content as the second argument, and properties as the third argument. made a few tests, one for html, another for my method for writing out the properties to a string, and another for writing nested elements

--- lib/htmlify.rb
+++ lib/htmlify.rb
@@ -1,7 +1,18 @@
 require "htmlify/version"
 
 module Htmlify
-  def div content
-    "<div>#{content}</div>"
+  def html tag, content, properties={}
+    if properties.any?
+      return "<#{tag}#{get_properties properties}>#{content}</#{tag}>"
+    end
+    "<#{tag}>#{content}</#{tag}>"
+  end
+
+  def get_properties properties
+    ps = ""
+    properties.each do |key, value|
+      ps = ps + " #{key}='#{value}'"
+    end
+    ps
   end
 end
--- spec/htmlify_spec.rb
+++ spec/htmlify_spec.rb
@@ -6,11 +6,46 @@ describe 'creating a div' do
   describe 'creating a div with content' do
     before(:each) do
       @div = "<div>Hello World</div>"
-      @divify = div "Hello World"
+      @divify = html :div, "Hello World"
     end
 
-    it 'creates a div' do
+    it 'creates a div with content' do
       expect(@divify).to eq(@div)
     end
   end
+
+  describe 'getting a list of properties' do
+    before(:each) do
+      @expected_list = " id=\'box\'"
+      @list = get_properties id: :box
+    end
+
+    it 'returns a list of properties as a string' do
+      expect(@list).to eq(@expected_list)
+    end
+  end
+
+  describe 'creating a div with properties and content' do
+    before(:each) do
+      @expected = "<div id='box' foo='bar'>Hello World</div>"
+      @div_with_properties = html :div, "Hello World", id: 'box', foo: 'bar'
+    end
+
+    it 'creates a div with properties and content' do
+      expect(@div_with_properties).to eq(@expected)
+    end
+  end
+
+  describe 'nested lists' do
+    before(:each) do
+      @expected_template = "<body><div>I'm a nested div!</div></body>"
+      @template = html :body, (
+                    html :div, "I'm a nested div!")
+    end
+
+    it 'returns an html template with nested elements' do
+      expect(@template).to eq(@expected_template)
+    end
+  end
+
 end
